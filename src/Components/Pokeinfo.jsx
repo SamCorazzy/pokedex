39b4080c7b8998db79087e3fde530465e3f3ae03
Samuel Perez Reyes <corazzy@hotmail.com>
2022-06-22 21:25:53 -0500

    Commit pokedex

---
+++ src/Components/Pokeinfo.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+const Pokeinfo = ({ data }) => {
+    // console.log(data);
+    return (
+        <>
+            {!data ? (
+                ""
+            ) : (
+                <>
+                    <h1 className={`${data.types[0].type.name}`}>{data.id}.- {data.name}</h1>
+
+                    <div className={`containerMio ${data.types[0].type.name}`}>
+                        <div className="row">
+                            <div className="col">
+                                <img
+                                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
+                                    alt=""
+                                />
+                            </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="row">
+                                    <img
+                                        className="imgMio"
+                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${data.id}.png`}
+                                        alt=""
+                                    />
+                                    <img
+                                        className="imgMio"
+                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
+                                        alt=""
+                                    />
+                                </div>
+                                    <div className="row"><img
+                                        className="imgMio"
+                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${data.id}.png`}
+                                        alt=""
+                                    />
+                                    <img
+                                        className="imgMio"
+                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${data.id}.png`}
+                                        alt=""
+                                    />
+                                    </div>
+                                    
+                                
+
+                                <div className="abilities row">
+                                    {data.abilities.map((poke) => {
+                                        return (
+                                            <>
+                                                <div className="group">
+                                                    <h2>{poke.ability.name}</h2>
+                                                </div>
+                                            </>
+                                        );
+                                    })}
+                                </div>
+                                <div className="base-stat row">
+                                    {data.stats.map((poke) => {
+                                        return (
+                                            <>
+                                                <h3>
+                                                    {poke.stat.name}: {poke.base_stat}
+                                                </h3>
+                                            </>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                            <div className="col">
+                                
+                            </div>
+                    </div>
+                </>
+            )}
+        </>
+    );
+};
+export default Pokeinfo;
